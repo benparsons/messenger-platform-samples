947cfd3d265351f9f24dc839df152d2b03250770
Ben Parsons <ben@bpulse.co.uk>
2017-04-02 22:28:17 +0100

    fix lookup

--- app.js
+++ app.js
@@ -369,7 +369,7 @@ function receivedPostback(event) {
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
   sendTextMessage(senderID, "Postback called: " + payload);
-  sendStateAsButton(senderID, parseInt(payload, 10));
+  sendStateAsButton(senderID, payload);
 }
 
 /*
